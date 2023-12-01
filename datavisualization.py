6e8db07fb6bf8827f6738e8f069294eb4ab26002
Vishnu_B_G_Bharadwaja <146937608+vishnubgb-ca@users.noreply.github.com>
2023-12-01 14:35:25 +0530

    Update datavisualization.py

--- datavisualization.py
+++ datavisualization.py
@@ -7,13 +7,11 @@ import pathlib
 import zipfile
 import random
 
-def open_random_image(path):
+def open_random_images(path):
     # Get a list of all files in the folder
     all_files = os.listdir(path)
-    random_image_file = random.choice(all_files)
-    image_path = os.path.join(path, random_image_file)
-    image = Image.open(image_path)
-    return image
+    random.shuffle(all_files)
+    return all_images[:4]
     
 
 def visualise_image():
@@ -21,26 +19,10 @@ def visualise_image():
     url_response = requests.get(url)
     with zipfile.ZipFile(BytesIO(url_response.content)) as z:
         z.extractall('.')
-    print(os.path.join(os.getcwd(),"Training/glioma_tumor"),"------------",os.listdir(os.path.join(os.getcwd(),"Training/glioma_tumor")))
-    path = pathlib.Path(os.path.join(os.getcwd(),"Training"))
-    # glioma_tumor_image_urls,meningioma_tumor_image_urls,no_tumor_image_urls,pituitory_tumor_image_urls = extract_data()
-    # glioma_tumor_response = requests.get(glioma_tumor_image_urls[0])
-    # meningioma_tumor_response = requests.get(meningioma_tumor_image_urls[0])
-    # no_tumor_image_response = requests.get(no_tumor_image_urls[0])
-    # pituitory_tumor_image_response = requests.get(pituitory_tumor_image_urls[0])
-    
-    glioma_tumor_image = open_random_image(os.path.join(os.getcwd(),"Training/glioma_tumor"))
-    meningioma_tumor_image = open_random_image(os.path.join(os.getcwd(),"Training/meningioma_tumor"))
-    no_tumor_image = open_random_image(os.path.join(os.getcwd(),"Training/no_tumor"))
-    pituitory_tumor_image = open_random_image(os.path.join(os.getcwd(),"Training/pituitory_tumor"))
-    glioma_tumor_image.save('glioma_tumor.jpg')
-    meningioma_tumor_image.save('meningioma_tumor.jpg')
-    no_tumor_image.save('no_tumor.jpg')
-    pituitory_tumor_image.save('pituitory_tumor.jpg')
-    #print(glioma_tumor_image.show())
-    #print(meningioma_tumor_image.show())
-    #print(no_tumor_image.show())
-    #print(pituitory_tumor_image.show())
-    return path
+    images = open_random_images(os.path.join(os.getcwd(),"Water_Bodies_Dataset_Split/train_images"))
+    for i in range(4):
+        images[i].save('sample'+str(i)+'.jpg')
+    #meningioma_tumor_image.save('meningioma_tumor.jpg')
+    return url
 
 visualise_image()
